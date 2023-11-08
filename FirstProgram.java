4dedcb43d8ebbfab58b45eb3d5078af8b74a4c66
himanshusharma52 <148312759+himanshusharma52@users.noreply.github.com>
2023-11-09 01:03:05 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class FirstProgram {
+	
+	public static void main(String[]args) {
+	  Scanner sc=new Scanner(System.in);
+      System.out.println("enter the number : ");
+	  double r=sc.nextDouble();
+	  double area=3.14*r*r;
+	  System.out.println(area);
+	}
+
+}
